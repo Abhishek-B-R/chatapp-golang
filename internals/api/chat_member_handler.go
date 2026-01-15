61dacec7a0733f72b7e989eb25fce33b96311fbf
Abhishek B R <byteblaster1468@gmail.com>
2026-01-15 21:16:10 +0530

    created chat_member handler and all its logics

---
+++ internals/api/chat_member_handler.go
@@ -0,0 +1,147 @@
+package api
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/Abhishek-B-R/chat-app-golang/internals/store"
+	"github.com/Abhishek-B-R/chat-app-golang/internals/utils"
+)
+
+type ChatMemberHandler struct {
+	chatMemberStore store.ChatMemberStore
+	logger *log.Logger
+}
+
+func NewChatMemberHandler(ChatMemberStore store.ChatMemberStore, logger *log.Logger) *ChatMemberHandler {
+	return &ChatMemberHandler{
+		chatMemberStore: ChatMemberStore,
+		logger: logger,
+	}
+}
+
+func (cmh *ChatMemberHandler) HandleAddMember(w http.ResponseWriter, r *http.Request) {
+	var params struct{
+		chatID, userID int64
+		role string
+	}
+	err := json.NewDecoder(r.Body).Decode(&params)
+	if err != nil {
+		cmh.logger.Printf("ERROR: decodingAddMember: %v\n",err)
+		utils.WriteJSON(w, http.StatusBadRequest, utils.Envelope{"error":"invalid request sent"})
+		return
+	}
+
+	err = cmh.chatMemberStore.AddMember(params.chatID, params.userID, params.role)
+	if err != nil {
+		cmh.logger.Printf("ERROR: addMember: %v\n",err)
+		utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error":"failed to add member"})
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, utils.Envelope{})
+}
+
+func (cmh *ChatMemberHandler) HandleRemoveMember(w http.ResponseWriter, r *http.Request) {
+	var params struct{
+		chatID, userID int64
+	}
+	err := json.NewDecoder(r.Body).Decode(&params)
+	if err != nil {
+		cmh.logger.Printf("ERROR: decodingRemoveMember: %v\n",err)
+		utils.WriteJSON(w, http.StatusBadRequest, utils.Envelope{"error":"invalid request sent"})
+		return
+	}
+
+	err = cmh.chatMemberStore.RemoveMember(params.chatID, params.userID)
+	if err != nil {
+		cmh.logger.Printf("ERROR: removeMember: %v\n",err)
+		utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error":"failed to remove member"})
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, utils.Envelope{})
+}
+
+func (cmh *ChatMemberHandler) HandleGetChatMembers(w http.ResponseWriter, r *http.Request){
+	chatID, err := utils.ReadParam(r, "chatID")
+	if err != nil {
+		cmh.logger.Printf("ERROR: decodingGetChatMembers: %v\n",err)
+		utils.WriteJSON(w, http.StatusBadRequest, utils.Envelope{"error":"invalid request sent"})
+		return
+	}
+
+	chatMembers, err := cmh.chatMemberStore.GetChatMembers(chatID)
+	if err != nil {
+		cmh.logger.Printf("ERROR: getChatMembers: %v\n",err)
+		utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error":"failed to retrieve chat members"})
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, utils.Envelope{"chat_members":chatMembers})
+}
+
+func (cmh *ChatMemberHandler) HandleGetUserRole(w http.ResponseWriter, r *http.Request) {
+	var params struct{
+		chatID, userID int64
+	}
+	err := json.NewDecoder(r.Body).Decode(&params)
+	if err != nil {
+		cmh.logger.Printf("ERROR: decodingGetUserRole: %v\n",err)
+		utils.WriteJSON(w, http.StatusBadRequest, utils.Envelope{"error":"invalid request sent"})
+		return
+	}
+
+	role, err := cmh.chatMemberStore.GetUserRole(params.chatID, params.userID)
+	if err != nil {
+		cmh.logger.Printf("ERROR: getUserRole: %v\n",err)
+		utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error":"failed to retrieve user role"})
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, utils.Envelope{"role":role})
+}
+
+func (cmh *ChatMemberHandler) HandleIsMember(w http.ResponseWriter, r *http.Request) {
+	var params struct{
+		chatID, userID int64
+	}
+	err := json.NewDecoder(r.Body).Decode(&params)
+	if err != nil {
+		cmh.logger.Printf("ERROR: decodingIsMember: %v\n",err)
+		utils.WriteJSON(w, http.StatusBadRequest, utils.Envelope{"error":"invalid request sent"})
+		return
+	}
+
+	isMember, err := cmh.chatMemberStore.IsMember(params.chatID, params.userID)
+	if err != nil {
+		cmh.logger.Printf("ERROR: isMember: %v\n",err)
+		utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error":"failed to retrieve is_member"})
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, utils.Envelope{"is_member":isMember})
+}
+
+func (cmh *ChatMemberHandler) HandleUpdateLastRead(w http.ResponseWriter, r *http.Request) {
+	var params struct{
+		chatID, userID,messageID int64
+	}
+	err := json.NewDecoder(r.Body).Decode(&params)
+	if err != nil {
+		cmh.logger.Printf("ERROR: decodingUpdateLastRead: %v\n",err)
+		utils.WriteJSON(w, http.StatusBadRequest, utils.Envelope{"error":"invalid request sent"})
+		return
+	}
+
+	err = cmh.chatMemberStore.UpdateLastRead(params.chatID, params.userID, params.messageID)
+	if err != nil {
+		cmh.logger.Printf("ERROR: updateLastRead: %v\n",err)
+		utils.WriteJSON(w, http.StatusInternalServerError, utils.Envelope{"error":"failed to update last read"})
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, utils.Envelope{})
+}
+
