20a3c4c57107cf3b6b4ae68ce3acd4fdd6a62ba7
Abhishek B R <byteblaster1468@gmail.com>
2026-01-18 19:59:43 +0530

    fixed a minor bug in sql code

--- migrations/00008_indexes_for_performance_optimization.sql
+++ migrations/00008_indexes_for_performance_optimization.sql
@@ -1,24 +1,16 @@
 -- +goose Up
 -- +goose StatementBegin
--- Composite index for finding user's chats sorted by last activity
-CREATE INDEX idx_chat_members_user_last_message 
-ON chat_members(user_id, chat_id);
-
 -- Partial index for active (non-deleted) messages only
 CREATE INDEX idx_messages_active 
 ON messages(chat_id, created_at DESC) 
 WHERE deleted_at IS NULL;
 
--- Index for finding DM chats between two users
--- This helps with GetOrCreateDM queries
-CREATE INDEX idx_dm_chats 
-ON chat_members(chat_id, user_id) 
-WHERE (SELECT is_group FROM chats WHERE id = chat_id) = false;
+-- Note: Removed the other two indexes as they're redundant
+-- idx_chat_members_user_last_message is covered by existing idx_chat_members_user_id
+-- idx_dm_chats can't use subquery in WHERE clause - we'll handle DM lookups differently
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP INDEX IF EXISTS idx_dm_chats;
 DROP INDEX IF EXISTS idx_messages_active;
-DROP INDEX IF EXISTS idx_chat_members_user_last_message;
 -- +goose StatementEnd
